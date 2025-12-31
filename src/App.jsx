0563bf959ce8dfd226b5156472f492fe61fc407a
Vamsee1317 <vamsee.achieversit@gmail.com>
2025-12-31 08:31:14 +0530

    feat: initialize React project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-blue-200 p-4 text-center text-white">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
